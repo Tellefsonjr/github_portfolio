e18b927f8e948028f2db66b4774b2c4809a837ca
Tellefsonjr <Timmeyellefson@gmail.com>
2018-12-11 00:11:01 -0800

    Updates

---
+++ precache-manifest.41b2e5e4247b6c01d0f4725533bb276f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "8c1f97bca62b6a51fefd",
+    "url": "/portfolio/static/js/main.8c1f97bc.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/portfolio/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "8c1f97bca62b6a51fefd",
+    "url": "/portfolio/static/css/main.df64eed2.chunk.css"
+  },
+  {
+    "revision": "3f47102ac22ea9efedd9c39de863b6f0",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.90c78f70168b5436141da5113933a3a5.js"
+  "/portfolio/precache-manifest.41b2e5e4247b6c01d0f4725533bb276f.js"
 );
 
 workbox.clientsClaim();
