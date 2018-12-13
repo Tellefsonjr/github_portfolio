e2d5cd8c0e94cb97798ccc6c56b894dc236cafe3
Tellefsonjr <Timmeyellefson@gmail.com>
2018-12-12 23:06:52 -0800

    adds about page stylings

--- src/components/About.js
+++ src/components/About.js
@@ -10,15 +10,14 @@ export class About extends Component {
       <div className="section">
         <div id="avatarWrapper">
           <img id="avatar" src={avatar} className="img-fluid rounded-circle mx-auto d-block" alt="Avatar" />
+          <div className="text-center text-light">
+            <h3 className="display-4"> Tim Ellefson </h3>
+            <h3 className="lead"><small> Full Stack Dev | API Enthusiast | Music Lover </small></h3>
+            <a href="/" ><span className="fab fa-github" /> </a>
+            <a href="/" ><span className="fab fa-linkedin" /> </a>
+            <a href="/" ><span className="fas fa-file-download" /> </a>
+          </div>
 
-        </div>
-        <div className="text-center text-light">
-          <h3> Tim Ellefson </h3>
-          <h3 className="lead"><small> Full Stack Dev | API Enthusiast | Music Lover </small></h3>
-          <h1><span className="fab fa-github" />
-            <span className="fab fa-linkedin" />
-            <span className="fas fa-file-download" />
-          </h1>
         </div>
       </div>
     );
