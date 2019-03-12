1266807fcf2f49e72237ffa89b6606424d4febee
Tellefsonjr <Timmeyellefson@gmail.com>
2019-03-11 23:17:31 -0700

    adds contact formhandling and state before email solution

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import axios from 'axios';
 import "../stylesheets/Contact.css";
 /**
  * Contact
@@ -7,8 +8,43 @@ import "../stylesheets/Contact.css";
  // TO DO: Use Context API Theme to adjust font and icon color...
 
 export class Contact extends Component { // eslint-disable-line react/prefer-stateless-function
+  state = {
+      name: "",
+      email: "",
+      message: "",
+      buttonText: "Send Message",
+      sent: false
+  }
 
-  // TO DO: Set state of contact collapse open close to change icon
+  formSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      buttonText: 'Sending...'
+    });
+
+    let data = {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message
+    };
+    axios.post('API_URI', data)
+      .then( res => {
+          this.setState({ sent: true }, this.resetForm())
+      })
+      .catch( () => {
+        console.log('Message not sent')
+      })
+    }
+    resetForm = () => {
+      this.setState({
+          name: '',
+          message: '',
+          email: '',
+          buttonText: 'Message Sent!'
+      })
+    }
+
+  // TO DO: Clean up bootstrap nesting...
 
   render() {
     return (
@@ -16,33 +52,29 @@ export class Contact extends Component { // eslint-disable-line react/prefer-sta
         <div id="contactSection" className="col-lg-12 col-xs-12 fluid-container text-center text-dark justify-content-center">
             <h3 className="display-4"> Contact Me </h3>
             <h3 className="lead"><small> If you've got a project that fits my skills, let me know!  </small></h3>
-            <form className="mx-auto col-lg-6 collapse" id="collapseExample">
-              <div className="form-group row">
-                <label for="name-input" className="col-2 col-offset-1 text-right col-form-label">Name</label>
-              <div className="col-10">
-                  <input className="form-control" type="text" placeholder="" id="name-input" />
-                </div>
-              </div>
-              <div className="form-group row">
-                <label for="email-input" className="col-2 col-offset-1 text-right col-form-label">Email</label>
-              <div className="col-10">
-                  <input className="form-control" type="email" placeholder="" id="email-input" />
-                </div>
-              </div>
-              <div className="form-group row">
-                <label for="subject-input" className="col-2 col-offset-1 text-right col-form-label">Subject:</label>
-              <div className="col-10">
-                  <input className="form-control" type="text" placeholder="" id="subject-input" />
-                </div>
-              </div>
-              <div className="form-group row">
-                <label for="body-input" className="col-2 col-offset-1 text-right col-form-label">Body:</label>
-              <div className="col-10">
-                  <textarea className="form-control" rows="4" placeholder="" id="body-input" />
-                </div>
-              </div>
-              <button type="submit" className="btn btn-default float-right">Submit</button>
-            </form>
+          <form className="mx-auto col-lg-6 collapse" id="collapseExample" onSubmit={ e => this.formSubmit(e) }>
+             <div className="form-group row">
+               <label for="name-input" className="col-2 col-offset-1 text-right col-form-label">Name</label>
+             <div className="col-10">
+                 <input className="form-control" type="text" placeholder="" id="name-input" onChange={e => this.setState({ name: e.target.value})} />
+               </div>
+             </div>
+             <div className="form-group row">
+               <label for="email-input" className="col-2 col-offset-1 text-right col-form-label">Email</label>
+             <div className="col-10">
+                 <input className="form-control" type="email" placeholder="" id="email-input" onChange={e => this.setState({ email: e.target.value})}/>
+               </div>
+             </div>
+             <div className="form-group row">
+               <label for="message-input" className="col-2 col-offset-1 text-right col-form-label">Message:</label>
+             <div className="col-10">
+                 <textarea className="form-control" rows="4" placeholder="" id="message-input" onChange={e => this.setState({ message: e.target.value})}/>
+               </div>
+             </div>
+             <button type="submit" className="btn btn-default float-right">{ this.state.buttonText }</button>
+           </form>
+
+
             <div className="col-12 float-right">
               <span className="fas fa-envelope text-dark" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"></span>
             </div>
@@ -52,6 +84,11 @@ export class Contact extends Component { // eslint-disable-line react/prefer-sta
     );
   }
 }
+//
+
+
+
+
 
 // Contact.propTypes = {
 //   prop: PropTypes.type.isRequired
